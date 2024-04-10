30f76bb85a5617cb8a46d2fc9a5bcdf3163b1bc1
Sumanth Hegde <sumanth.hegde@siemens.com>
2024-04-10 14:41:08 +0530

    file added

---
+++ src/com/sumanth/basic/SumOfRoundNumbers.java
@@ -0,0 +1,28 @@
+//https://codeforces.com/contest/1352/problem/A
+
+
+package com.sumanth.basic;
+
+import java.util.Scanner;
+
+public class SumOfRoundNumbers {
+    public static void main (String[] args) {
+        System.out.println("Number of inputs : ");
+        Scanner scanner = new Scanner(System.in);
+        
+        var n = scanner.nextInt();
+        var numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the number : ");
+            numbers[i]   = scanner.nextInt();
+        }
+        
+//
+//        for(int i=0;i<numbers.length;i++){
+//            int number = numbers[i];
+//            int count = 0;
+//            while()
+//
+//        }
+    }
+}
