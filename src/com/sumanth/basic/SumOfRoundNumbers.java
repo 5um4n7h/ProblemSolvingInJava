29ff1aaf38fe9aab9b427b9a95f4deb64e7c2ec4
Sumanth Hegde <sumanthhegde13@gmail.com>
2024-04-20 20:00:50 +0530

    SumOfRoundNumbers.java added

--- src/com/sumanth/basic/SumOfRoundNumbers.java
+++ src/com/sumanth/basic/SumOfRoundNumbers.java
@@ -3,26 +3,43 @@
 
 package com.sumanth.basic;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SumOfRoundNumbers {
-    public static void main (String[] args) {
-        System.out.println("Number of inputs : ");
+    public static void main(String[] args) {
+
+
         Scanner scanner = new Scanner(System.in);
-        
-        var n = scanner.nextInt();
-        var numbers = new int[n];
-        for (int i = 0; i < n; i++) {
+        System.out.println("Number of inputs : ");
+        int t = scanner.nextInt(); // number of test cases
+
+        while (t-- > 0) {
             System.out.println("Enter the number : ");
-            numbers[i]   = scanner.nextInt();
+            int n = scanner.nextInt(); // the number to represent
+
+            List<Integer> roundNumbers = new ArrayList<>();
+            int placeValue = 1; // to track place value (1, 10, 100, 1000, ...)
+
+            while (n > 0) {
+                int digit = n % 10;
+                if (digit > 0) {
+                    roundNumbers.add(digit * placeValue);
+                }
+                n /= 10;
+                placeValue *= 10;
+            }
+
+            // Output result for this test case
+            System.out.print(roundNumbers.size());
+            for (int num : roundNumbers) {
+                System.out.print(" " + num);
+            }
+            System.out.println();
         }
-        
-//
-//        for(int i=0;i<numbers.length;i++){
-//            int number = numbers[i];
-//            int count = 0;
-//            while()
-//
-//        }
+
+        scanner.close();
     }
 }
+
