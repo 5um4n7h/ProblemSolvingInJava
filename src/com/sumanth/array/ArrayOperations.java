b3c2ed2bf449c23a44ff4263088e63f09e13440d
Sumanth Hegde <sumanthhegde13@gmail.com>
2024-10-02 19:42:10 +0530

    file added

---
+++ src/com/sumanth/array/ArrayOperations.java
@@ -0,0 +1,25 @@
+package com.sumanth.array;
+
+import java.util.Arrays;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (var i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
+
+        System.out.println(Arrays.toString(arr));
+        //prints the memory reference
+        System.out.println(arr);
+        //delete at 5th
+
+        for (int i = 4; i < 9; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[9] = 0;
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
